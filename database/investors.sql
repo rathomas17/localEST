25eb1c51e18a750a9769de1700c49ebe76f00c59
aaronfuhrer <aaron.fuhrer@gmail.com>
2017-10-21 13:56:43 -0400

    investors.sql

---
+++ database/investors.sql
@@ -0,0 +1,9 @@
+DROP TABLE if EXISTS investor
+
+CREATE TABLE investor(userID VARCHAR(10),
+ firstName VARCHAR(25), lastName VARCHAR(25),
+  companyName VARCHAR(25), availableFunding VARCHAR(10),
+   interest1 VARCHAR(20), interest2 VARCHAR(20), interest3 VARCHAR(20),
+   avgInvestment VARCHAR(10), phone VARCHAR(20), email VARCHAR(50),
+   twitter VARCHAR(25), instagram VARCHAR(50), facebook VARCHAR(50),
+    linkedIn VARCHAR(50));
