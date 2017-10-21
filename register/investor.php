2061213a044680a92d92c837bffce2852b98fa26
rathomas17 <rathomas17@gmail.com>
2017-10-21 19:43:32 -0400

    pulling

--- register/investor.php
+++ register/investor.php
@@ -13,19 +13,19 @@ if (!$conn) {
 $userName =  $_REQUEST['userName'];
 $password =  $_REQUEST['password'];
 $first_name =  $_REQUEST['first_name'];
-$last_name = $_REQUEST['last_name']
-$companyName = $_REQUEST['companyName']
-$availableFunding = $_REQUEST['availableFunding']
-$interest1 = $_REQUEST['interest1']
-$interest2 = $_REQUEST['interest2']
-$interest3 = $_REQUEST['interest3']
-$avgInvestment = $_REQUEST['avgInvestment']
-$phone = $_REQUEST['phone']
-$email = $_REQUEST['email']
-$twitter = $_REQUEST['twitter']
-$instagram = $_REQUEST['instagram']
-$facebook = $_REQUEST['facebook']
-$linkedIn = $_REQUEST['linkedIn']
+$last_name = $_REQUEST['last_name'];
+$companyName = $_REQUEST['companyName'];
+$availableFunding = $_REQUEST['availableFunding'];
+$interest1 = $_REQUEST['interest1'];
+$interest2 = $_REQUEST['interest2'];
+$interest3 = $_REQUEST['interest3'];
+$avgInvestment = $_REQUEST['avgInvestment'];
+$phone = $_REQUEST['phone'];
+$email = $_REQUEST['email'];
+$twitter = $_REQUEST['twitter'];
+$instagram = $_REQUEST['instagram'];
+$facebook = $_REQUEST['facebook'];
+$linkedIn = $_REQUEST['linkedIn'];
 
 
 $sql = "INSERT INTO investor (userName, password, firstName, lastName, companyName, availableFunding,
