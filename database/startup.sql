ed3fe728c11457b5c16220d56f76ad08d0333d89
Aaron <aaronfuhrer@Aarons-MacBook-Pro-2.local>
2017-10-21 13:49:18 -0400

    startup.sql

---
+++ database/startup.sql
@@ -0,0 +1,18 @@
+DROP TABLE IF EXISTS startup;
+
+CREATE TABLE startup (
+    Org_ID varchar(10),
+    Name varchar(25),
+    Street varchar(25),
+    City varchar(25),
+    State varchar(2),
+    Zip varchar(5),
+    Phone varchar(10),
+    Email varchar(50),
+    Website varchar(69),
+    Need1 varchar(50),
+    Need2 varchar(50),
+    Need3 varchar(50),
+    Need4 varchar(50),
+    Need5 varchar(50)
+);
